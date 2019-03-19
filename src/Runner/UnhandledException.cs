8ceadeedf1a1e4b6c5952569a4c875bdb2e31afb
Boris Nadezhdin <Nadezhdin.BV@gazprom-neft.ru>
2019-03-19 18:47:14 +0500

    unhandled exception test case

--- src/Runner/UnhandledException.cs
+++ src/Runner/UnhandledException.cs
@@ -6,7 +6,28 @@ namespace Runner
     {
         public void Run(string[] args)
         {
-            throw new NotImplementedException();
+            var inputs = new[]
+            {
+                new Input{A = 1, B = 2 },
+                new Input{A = 2, B = 3 },
+                new Input{A = 3, B = 0 }
+            };
+            foreach (var input in inputs)
+            {
+                PrintResult(input);
+            }
+        }
+
+        private void PrintResult(Input input)
+        {
+            var result = input.A / input.B;
+            Console.WriteLine($"A/B={result}");
+        }
+
+        private class Input
+        {
+            public int A;
+            public int B;
         }
     }
 }
